98bcbb3bd889a810073b3c0b8dbc6875e1d670ed
Claudia Hernandez-Armenta <cihernand@users.noreply.github.com>
2020-11-11 20:45:34 -0600

    Add files via upload
    bash file to create directory structure

---
+++ init.sh
@@ -0,0 +1,8 @@
+
+mkdir data  docs  models  notebooks references  reports  src
+mkdir  data/external data/interim data/processed data/raw data/figures
+mkdir reports/figures
+mkdir notebooks/exploratory notebooks/report notebooks/draft notebooks/notebook2src
+
+mkdir src/data src/features src/models src/visualization
+touch LICENSE Makefile README.md requirements.txt config_venv.txt .gitignore
